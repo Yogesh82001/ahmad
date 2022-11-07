d40f19a63b035852a43546e8a76f4ec0fb0e8303
Yogesh Panchal <yogeshpanchal28082001@gmail.com>
2022-11-07 12:55:18 +0530

    First Commit

---
+++ ahemad.java
@@ -0,0 +1,13 @@
+/**
+ * ahemad
+ */
+public class ahemad {
+  public static void main(String[] args) {
+        A obj = new A();
+  }
+}
+class A{
+    public A() {
+        System.out.println("Inside A");
+    }
+}
